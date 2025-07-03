1f3a3ae5ea002ae3491d48fdd8b17657e0d16569
zScalper <jeanpaul-15.pol@hotmail.com>
2025-07-03 10:45:37 -0500

    dsadas

--- src/test/java/com/dyaminimarket/DyAMinimarketApplicationTests.java
+++ src/test/java/com/dyaminimarket/DyAMinimarketApplicationTests.java
@@ -3,11 +3,11 @@ package com.dyaminimarket;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DyAMinimarketApplicationTests {
+//@SpringBootTest
+//class DyAMinimarketApplicationTests {
+//
+//  @Test
+//  void contextLoads() {
+//  }
+//}
 
-	@Test
-	void contextLoads() {
-	}
-
-}
